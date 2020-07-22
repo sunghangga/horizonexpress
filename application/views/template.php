be84667939faeb2539e5ee2058774eb1082e2644
Angga Pramana Putra <sunghangga@gmail.com>
2020-07-22 11:30:43 +0800

    fix template

--- application/views/template.php
+++ application/views/template.php
@@ -104,7 +104,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-danger elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link bg-red">
+    <a href="<?php echo base_url('index.php/beranda') ?>" class="brand-link bg-red">
       <img src="<?php echo base_url() ?>template/dist/img/logo-white.png" alt="AdminLTE Logo" class="brand-image"
            style="opacity: .8">
       <span class="brand-text font-weight-light"><b>Horizon</b> Express</span>
@@ -403,12 +403,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- <script src="<?php echo base_url() ?>template/adminlte/dist/js/pages/dashboard.js"></script> -->
 <!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url() ?>template/adminlte/dist/js/demo.js"></script>
+<!-- <script src="<?php echo base_url() ?>template/adminlte/dist/js/demo.js"></script> -->
 <!-- datatables -->
 <script src="<?php echo base_url() ?>template/adminlte/plugins/datatables/jquery.dataTables.js"></script>
 <script src="<?php echo base_url() ?>template/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
 <script src="<?php echo base_url() ?>template/adminlte/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-<script src="<?php echo base_url() ?>template/adminlte/dist/js/adminlte.min.js"></script>
+<!-- <script src="<?php echo base_url() ?>template/adminlte/dist/js/adminlte.min.js"></script> -->
 <script src="<?php echo base_url('template/adminlte/plugins/select2/js/select2.full.min.js')?>"></script>
 <script src="<?php echo base_url('template/plugins/sweetalert/sweetalert.min.js')?>"></script>
 
